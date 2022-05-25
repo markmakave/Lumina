e18d0f7dd3762399d5f939bd47981192d7cb5f1f
markmakave <markmakave@gmail.com>
2022-05-25 18:25:01 +0300

    Initial Commit

---
+++ Lumina/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lumina
+//
+//  Created by Марк Мохов on 25.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lumina/LuminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaApp.swift
+//  Lumina
+//
+//  Created by Марк Мохов on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LuminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
